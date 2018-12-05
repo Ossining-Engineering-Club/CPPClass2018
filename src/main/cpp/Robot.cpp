4bdc2617970b8d0972ee638a8d3d443461d05e6b
vikesh <vsubramanian0208@students.ossiningufsd.org>
2018-12-05 15:19:32 -0500

    first commit

---
+++ src/main/cpp/Robot.cpp
@@ -0,0 +1,43 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "Robot.h"
+
+Robot::Robot() {
+}
+
+void Robot::RobotInit() {
+}
+
+/**
+ * This autonomous (along with the chooser code above) shows how to select
+ * between different autonomous modes using the dashboard. The sendable chooser
+ * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+ * remove all of the chooser code and uncomment the GetString line to get the
+ * auto name from the text box below the Gyro.
+ *
+ * You can add additional auto modes by adding additional comparisons to the
+ * if-else structure below with additional strings. If using the SendableChooser
+ * make sure to add them to the chooser code above as well.
+ */
+void Robot::Autonomous() {
+}
+
+/**
+ * Runs the motors with arcade steering.
+ */
+void Robot::OperatorControl() {
+}
+
+/**
+ * Runs during test mode
+ */
+void Robot::Test() {}
+
+#ifndef RUNNING_FRC_TESTS
+START_ROBOT_CLASS(Robot)
+#endif
